5d6ddad3fe78cc0fe5141cc5645d0581a34ba5ea
Daniel Covello <daniel.covello@prolucid.ca>
2016-04-21 21:43:57 +0000

    initial submission of base storm image files

---
+++ storm_base/start-storm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Configure mandatory storm.yaml settings
+ZK_LINES=
+for i in $ZK_SERVERS ; do
+    ZK_LINES="$ZK_LINES\\n - "\"$i\"
+done
+
+sed -i -e "s/storm.zookeeper.servers:.*/storm.zookeeper.servers: $ZK_LINES/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/nimbus.host:.*/nimbus.host: $NIMBUS_HOST/g" $STORM_HOME/conf/storm.yaml
+sed -i -e "s/storm.zookeeper.port:.*/storm.zookeeper.port: $ZK_PORT/g" $STORM_HOME/conf/storm.yaml
+
+exec storm %STORMAPP%
